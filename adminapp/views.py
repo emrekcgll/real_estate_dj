0319661c5e2c2b04756172400d6f396b232d44d4
emre <emrekcgll@gmail.com>
2023-09-14 22:55:34 +0300

    address import operations

--- adminapp/views.py
+++ adminapp/views.py
@@ -6,7 +6,7 @@ from adminapp.models import City, Country, Neighbourhood, Region
 
 # Create your views here.
 
-def import_country(request):
+def import_address_data(request):
     if request.method == "POST":
         csv_file = request.FILES.get("file")
         if csv_file:
@@ -14,55 +14,36 @@ def import_country(request):
             reader = csv.reader(csv_file)
             next(reader)
             for row in reader:
-                country_name_csv = row[0].strip()
-                country = Country(country_name=country_name_csv)
-                country.save()
+                country_name_csv = row[0].strip().title()
+                city_name_csv = row[1].strip().title()
+                region_name_csv = row[2].strip().title()
+                neighbourhood_name_csv = row[3].strip().title()
+                neighbourhood_zip_csv = row[4].strip()
+                
+                try:
+                    Country.objects.get(country_name=country_name_csv)
+                except Country.DoesNotExist:
+                    country = Country(country_name=country_name_csv)
+                    country.save()
+                try:
+                    City.objects.get(city_name=city_name_csv)
+                except City.DoesNotExist:
+                    city = City(country=country, city_name=city_name_csv)
+                    city.save()
+
+                try:
+                    Region.objects.get(region_name=region_name_csv)
+                except Region.DoesNotExist:
+                    region = Region(city=city ,region_name=region_name_csv)
+                    region.save()
+
+                try:
+                    Neighbourhood.objects.get(neighbourhood_name=neighbourhood_name_csv)
+                except Neighbourhood.DoesNotExist:
+                    neighbourhood = Neighbourhood(region=region ,neighbourhood_name=neighbourhood_name_csv, neighbourhood_zip=neighbourhood_zip_csv) 
+                    neighbourhood.save()
     return redirect('index')
     
-def import_city(request):
-    if request.method == "POST":
-        csv_file = request.FILES.get("file")
-        if csv_file:
-            csv_file = io.TextIOWrapper(csv_file, encoding="utf-8")
-            reader = csv.reader(csv_file)
-            next(reader)
-            for row in reader:
-                city_name_csv = row[1].strip().title()
-                country_id = row[2].strip()
-                country = City(country_id=country_id, city_name=city_name_csv)
-                country.save()
-    return redirect('index')
-
-def import_region(request):
-    if request.method == "POST":
-        csv_file = request.FILES.get("file")
-        if csv_file:
-            csv_file = io.TextIOWrapper(csv_file, encoding="utf-8")
-            reader = csv.reader(csv_file)
-            next(reader)
-            for row in reader:
-                region_name_csv = row[2].strip().title()
-                city_id_csv = row[1].strip()
-                region = Region(city_id=city_id_csv, region_name=region_name_csv)
-                region.save()
-    return redirect('index')
-
-def import_neighbourhood(request):
-    if request.method == "POST":
-        csv_file = request.FILES.get("file")
-        if csv_file:
-            csv_file = io.TextIOWrapper(csv_file, encoding="utf-8")
-            reader = csv.reader(csv_file)
-            next(reader)
-            for row in reader:
-                neighbourhood_name_csv = row[2].strip()
-                neighbourhood_zip = row[3].strip()
-                region_id_csv = row[1].strip()
-                neighbourhood = Neighbourhood(region_id=region_id_csv, neighbourhood_name=neighbourhood_name_csv, neighbourhood_zip=neighbourhood_zip)
-                neighbourhood.save()
-    return redirect('index')
-
-
 def index(request):
     if request.method == "POST":
         csv_file = request.FILES.get("file")
