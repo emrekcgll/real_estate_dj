d66b1eeddf1e471c989daf694a28a6ef386967af
emre <emrekcgll@gmail.com>
2023-09-17 13:57:00 +0300

    -

--- modelapp/urls.py
+++ modelapp/urls.py
@@ -6,18 +6,23 @@ urlpatterns = [
     path("import_address_data/",views.import_address_data, name="import_address_data"),
 
 
-    path("create_estate_type/", views.create_estate_type, name="create_estate_type"),
-    path("create_estate_status/", views.create_estate_status, name="create_estate_status"),
-    path("create_from_who/", views.create_from_who, name="create_from_who"),
-    path("create_room_count/", views.create_room_count, name="create_room_count"),
-    
     path("show_estate_type/", views.show_estate_type, name="show_estate_type"),
     path("show_estate_status/", views.show_estate_status, name="show_estate_status"),
     path("show_from_who/", views.show_from_who, name="show_from_who"),
     path("show_room_count/", views.show_room_count, name="show_room_count"),
 
-    path("delete_estate_type/", views.delete_estate_type, name="delete_estate_type"),
-    path("delete_estate_status/", views.delete_estate_status, name="delete_estate_status"),
-    path("delete_from_who/", views.delete_from_who, name="delete_from_who"),
-    path("delete_room_count/", views.delete_room_count, name="delete_room_count"),
+    path("create_estate_type/", views.create_estate_type, name="create_estate_type"),
+    path("create_estate_status/", views.create_estate_status, name="create_estate_status"),
+    path("create_from_who/", views.create_from_who, name="create_from_who"),
+    path("create_room_count/", views.create_room_count, name="create_room_count"),
+
+    path("delete_estate_type/<int:pk>/", views.delete_estate_type, name="delete_estate_type"),
+    path("delete_estate_status/<int:pk>/", views.delete_estate_status, name="delete_estate_status"),
+    path("delete_from_who/<int:pk>/", views.delete_from_who, name="delete_from_who"),
+    path("delete_room_count/<int:pk>/", views.delete_room_count, name="delete_room_count"),
+
+    path("update_estate_type/<int:pk>/", views.update_estate_type, name="update_estate_type"),
+    path("update_estate_status/<int:pk>/", views.update_estate_status, name="update_estate_status"),
+    path("update_from_who/<int:pk>/", views.update_from_who, name="update_from_who"),
+    path("update_room_count/<int:pk>/", views.update_room_count, name="update_room_count"),
 ]
--- modelapp/views.py
+++ modelapp/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from adminapp.models import City, County, EstateStatus, EstateType, FromWho, Neighbourhood, Region, RoomCount
 from modelapp.forms import EstateStatusForm, EstateTypeForm, FromWhoForm, RoomCountForm
@@ -47,6 +47,58 @@ def import_address_data(request):
                         region=region, neighbourhood_name=neighbourhood_name_csv, neighbourhood_zip=neighbourhood_zip_csv)
                     neighbourhood.save()
     return redirect('import_operations')
+
+
+
+
+def show_estate_type(request):
+    estate_types = EstateType.objects.all()
+    data = []
+    for i in estate_types:
+        data.append({
+            "pk": i.pk,
+            "estate_type": i.estate_type
+        })
+    response = {"data": data}
+    return JsonResponse(response)
+
+
+def show_estate_status(request):
+    estate_status = EstateStatus.objects.all()
+    data = []
+    for i in estate_status:
+        data.append({
+            "pk": i.pk,
+            "estate_status": i.estate_status
+        })
+    response = {"data": data}
+    return JsonResponse(response)
+
+
+def show_from_who(request):
+    from_whos = FromWho.objects.all()
+    data = []
+    for i in from_whos:
+        data.append({
+            "pk": i.pk,
+            "from_who": i.from_who
+        })
+    response = {"data": data}
+    return JsonResponse(response)
+
+
+def show_room_count(request):
+    room_counts = RoomCount.objects.all()
+    data = []
+    for i in room_counts:
+        data.append({
+            "pk": i.pk,
+            "room_count": i.room_count
+        })
+    response = {"data": data}
+    return JsonResponse(response)
+
+
 
 
 def create_estate_type(request):
@@ -97,61 +149,90 @@ def create_room_count(request):
     return render(request, "modelapp/create_room_count.html", {"form": form})
 
 
-def show_estate_type(request):
-    estate_types = EstateType.objects.all()
-    data = []
-    for i in estate_types:
-        data.append({
-            "estate_type": i.estate_type,
-        })
-    response = {"data": data}
-    return JsonResponse(response)
-
-
-def show_estate_status(request):
-    estate_status = EstateStatus.objects.all()
-    data = []
-    for i in estate_status:
-        data.append({
-            "estate_status": i.estate_status,
-        })
-    response = {"data": data}
-    return JsonResponse(response)
-
-
-def show_from_who(request):
-    from_whos = FromWho.objects.all()
-    data = []
-    for i in from_whos:
-        data.append({
-            "from_who": i.from_who,
-        })
-    response = {"data": data}
-    return JsonResponse(response)
-
-
-def show_room_count(request):
-    room_counts = RoomCount.objects.all()
-    data = []
-    for i in room_counts:
-        data.append({
-            "room_count": i.room_count,
-        })
-    response = {"data": data}
-    return JsonResponse(response)
 
 
 def delete_estate_type(request, pk):
-    pass
+    value = get_object_or_404(EstateType, pk=pk)
+    value.delete()
+    messages.success(request, "Deleted successfully.")
+    return redirect("create_estate_type")
 
 
 def delete_estate_status(request, pk):
-    pass
+    value = get_object_or_404(EstateStatus, pk=pk)
+    value.delete()
+    messages.success(request, "Deleted successfully.")
+    return redirect("create_estate_status")
 
 
 def delete_from_who(request, pk):
-    pass
+    value = get_object_or_404(FromWho, pk=pk)
+    value.delete()
+    messages.success(request, "Deleted successfully.")
+    return redirect("create_from_who")
 
 
 def delete_room_count(request, pk):
-    pass
+    value = get_object_or_404(RoomCount, pk=pk)
+    value.delete()
+    messages.success(request, "Deleted successfully.")
+    return redirect("create_room_count")
+
+
+
+
+def update_estate_type(request, pk):
+    value = get_object_or_404(EstateType, pk=pk)
+    if request.method == "POST":
+        form = EstateTypeForm(request.POST, instance=value)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated successfully.")
+            return redirect("create_estate_type")
+        else:
+            messages.error(request, "Something went wrong.")
+    form = EstateTypeForm(instance=value)
+    return render(request, "modelapp/update_estate_type.html", {"form": form})
+
+
+def update_estate_status(request, pk):
+    value = get_object_or_404(EstateStatus, pk=pk)
+    if request.method == "POST":
+        form = EstateStatusForm(request.POST, instance=value)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated successfully.")
+            return redirect("create_estate_status")
+        else:
+            messages.error(request, "Something went wrong.")
+    form = EstateStatusForm(instance=value)
+    return render(request, "modelapp/update_estate_status.html", {"form": form})
+
+
+def update_from_who(request, pk):
+    value = get_object_or_404(FromWho, pk=pk)
+    if request.method == "POST":
+        form = FromWhoForm(request.POST, instance=value)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated successfully.")
+            return redirect("create_from_who")
+        else:
+            messages.error(request, "Something went wrong.")
+    form = FromWhoForm(instance=value)
+    return render(request, "modelapp/update_from_who.html", {"form": form})
+
+
+def update_room_count(request, pk):
+    value = get_object_or_404(RoomCount, pk=pk)
+    if request.method == "POST":
+        form = RoomCountForm(request.POST, instance=value)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated successfully.")
+            return redirect("create_room_count")
+        else:
+            messages.error(request, "Something went wrong.")
+    form = RoomCountForm(instance=value)
+    return render(request, "modelapp/update_room_count.html", {"form": form})
+
